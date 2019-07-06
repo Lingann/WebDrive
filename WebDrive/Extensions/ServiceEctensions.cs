983a690e40bd890c4b97398c418f8d99b00d0711
Lingann <lingann.m@gmail.com>
2019-07-06 17:17:12 +0800

    更新跨域设置

---
+++ WebDrive/Extensions/ServiceEctensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
+
+namespace WebDrive.Extensions
+{
+    public static class ServiceEctensions
+    {
+        public static void ConfigCors(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+            services.AddMvc();
+        }
+    }
+}
